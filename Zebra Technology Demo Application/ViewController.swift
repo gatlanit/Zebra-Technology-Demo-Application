8f47317b7e7c5df82cebede032d693da238a3851
Nitin Gatla <gatlanit@gmail.com>
2025-08-27 16:56:08 -0500

    Initial Commit

---
+++ Zebra Technology Demo Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zebra Technology Demo Application
+//
+//  Created by Nitin G on 8/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Zebra Technology Demo ApplicationTests/Zebra_Technology_Demo_ApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zebra_Technology_Demo_ApplicationTests.swift
+//  Zebra Technology Demo ApplicationTests
+//
+//  Created by Nitin G on 8/27/25.
+//
+
+import Testing
+@testable import Zebra_Technology_Demo_Application
+
+struct Zebra_Technology_Demo_ApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
